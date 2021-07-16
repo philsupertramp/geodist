77fb558660f5493db401639936c23d57f7cc867b
Philipp Zettl <philipp@godesteem.de>
2021-07-16 18:13:40 +0200

    build class to read cities from file

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+import json
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class City:
+    name: str
+    place_id: str
+    country_name: str
+    country_place_id: str
+    country_region: str
+
+    @classmethod
+    def from_file(cls, filename) -> List['City']:
+        with open(filename, 'r') as file:
+            objs = json.loads(file.read())
+
+        return [cls(**obj) for obj in objs]
 
 
 if __name__ == '__main__':
-    pass
+    cities = City.from_file('cities.json')
